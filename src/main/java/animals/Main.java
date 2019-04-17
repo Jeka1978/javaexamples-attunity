876b7f5c8670d034a63590dcc09407ac36f04ce1
jeka1978 <kit2009>
2019-04-17 16:59:09 +0300

    lab - guess game   String s = JOptionPane.showInputDialog("intput your num");         int x = Integer.parseInt(s);
            in the end of game number of tries should be printed

--- src/main/java/animals/Main.java
+++ src/main/java/animals/Main.java
@@ -1,11 +1,19 @@
 package animals;
 
+
+import java.awt.*;
+
+import static java.lang.Math.*;
+
 /**
  * @author Evgeny Borisov
  */
 public class Main {
     public static void main(String[] args) {
 
+
+        double sin = sin(30);
+        System.out.println("sin = " + sin);
 
         AnimalFactory factory = new AnimalFactory();
         for (int i = 0; i < 5; i++) {
--- src/main/java/lab1/Main.java
+++ src/main/java/lab1/Main.java
@@ -1,5 +1,6 @@
 package lab1;
 
+import javax.swing.*;
 import java.util.Arrays;
 
 /**
@@ -7,6 +8,10 @@ import java.util.Arrays;
  */
 public class Main {
     public static void main(String[] args) {
+
+        String s = JOptionPane.showInputDialog("intput your num");
+        int x = Integer.parseInt(s);
+
         double[] doubles = {2.5, 3.3, 3};
         int[] ints = new int[doubles.length];
 
@@ -14,6 +19,7 @@ public class Main {
             ints[i] = (int) doubles[i];
         }
 
+        System.gc();
         Arrays.stream(ints).forEach(System.out::println);
 
     }
